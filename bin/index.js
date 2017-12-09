00bbd0680836e644dd0f8c18c65ee0564d2ebc3a
DavtyanErik <erikdavtyan@gmail.com>
2017-12-09 18:49:30 +0400

    Fix linting, modulize code

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 
-'use strict';
 
 // validate that used node version is supported
-var semver = require('semver');
-var ver = process.versions.node;
-ver = ver.split('-')[0]; // explode and truncate tag from version
+const semver = require('semver');
+
+const ver = process.versions.node.split('-')[0]; // explode and truncate tag from version
 
 if (semver.satisfies(ver, '>=8.2.0')) {
   require('../build/lectures.js').run();
 } else {
-  console.log(
-    require('chalk').red(
-      'Node version ' + ver + ' is not supported, please use Node.js 6.0 or higher.'
-    )
-  );
+  console.log(require('chalk').red(`Node version ${ver} is not supported, please use Node.js 6.0 or higher.`));
   process.exit(1);
 }
